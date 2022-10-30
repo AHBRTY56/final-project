505228791150832c412df98163cf40e67a9c8e5a
AHBRTY56 <116176575+AHBRTY56@users.noreply.github.com>
2022-10-30 19:18:48 +0100

    showing current temp

--- src/tech.js
+++ src/tech.js
@@ -1,1 +1,17 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let wetElement = document.querySelector("#wet");
+  wetElement.innerHTML = response.data.temperature.humidity;
 
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
+let apiKey = "bc7dota507232177ccef048eb1a1ae2a";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Kharkiv&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
